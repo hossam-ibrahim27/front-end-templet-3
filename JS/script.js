ffe01ec3871de105428d65635c402eecfc4f9f68
Hossam Ibrahim <enghossamibrahimhassan@gmail.com>
2024-11-07 17:50:18 -0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+var addcarthome = document.getElementById("addcarthome")
+var addcartaway = document.getElementById("addcartaway")
+var addcartthird = document.getElementById("addcartthird")
+addcarthome.onclick = function(){
+    window.alert("Mens Home Authentic Shirt 24/25 White Will Add to cart !")
+}
+addcartaway.onclick = function(){
+    window.alert("Mens Home Authentic Shirt 24/25 Orange Will Add to cart !")
+}
+addcartthird.onclick = function(){
+    window.alert("Mens Home Authentic Shirt 24/25 Charcoal Will Add to cart !")
+}
